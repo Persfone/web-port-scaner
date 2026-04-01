90affb555a0f12f8421a994ac3bc1ebe7bcc9d5a
Persfone <persfone240@gmail.com>
2026-04-01 22:03:37 +0000

    pagina funcional pero muy muy mejorable solo tcp

--- backend/scanner.py
+++ backend/scanner.py
@@ -1,23 +1,44 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import socket
+from concurrent.futures import ThreadPoolExecutor
 
 app = Flask(__name__)
+CORS(app)
+
+
+def scan_port(ip, port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(0.5)
+
+        result = s.connect_ex((ip, port))
+        s.close()
+
+        if result == 0:
+            return port
+
+    except:
+        return None
+
 
 def scan_ports(target):
 
     open_ports = []
 
-    for port in range(20, 100):
+    try:
+        ip = socket.gethostbyname(target)
+    except:
+        return []
 
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(1)
+    with ThreadPoolExecutor(max_workers=50) as executor: #concurrencia de hasta 50
+        results = executor.map(lambda p: scan_port(ip, p), range(1, 1024)) # deteccion de puertos tipicos y puertos bien conocidos
 
-        result = s.connect_ex((target, port))
+    for r in results:
+        if r:
+            open_ports.append(r)
 
-        if result == 0:
-            open_ports.append(port)
-
-        s.close()
+    open_ports.sort()
 
     return open_ports
 
@@ -25,11 +46,23 @@ def scan_ports(target):
 @app.route("/scan")
 def scan():
 
-    ip = request.args.get("ip")
+    target = request.args.get("ip")
 
-    ports = scan_ports(ip)
+    if not target:
+        return jsonify({"error": "No target provided"})
 
-    return jsonify(ports)
+    try:
+        ip = socket.gethostbyname(target)
+    except:
+        return jsonify({"error": "Invalid domain or DNS error"})
+
+    ports = scan_ports(target)
+
+    return jsonify({ 
+        "target": target.upper(),
+        "ip": ip, 
+        "open_ports": ports
+    })
 
 
 app.run(host="0.0.0.0", port=5000)
